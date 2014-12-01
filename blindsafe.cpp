704b64b11c535205d326d45f2b6cffecaa324df4
Ross Yeager <ross.yeager@berkeley.edu>
2014-12-01 02:58:16 -0800

    Cleaned up and updated main code
    Cleaned up the code...accelerometer is configuring properly but the
    accelerometer isr's are not getting properly called.

--- blindsafe.cpp
+++ blindsafe.cpp
@@ -28,31 +28,34 @@
 #include <avr/sleep.h>
 #include <util/delay.h>
 
-#include "i2c.h"
+//#include "i2c.h"
 #define RDV_DEBUG
 
 //PIN DEFINITIONS
 
 //blind spot detector pins
 #define CE_REG		PORTB1	//OUTPUT
-#define CE_REG1		PORTB2	//OUTPUT
-#define INT1_ACC	PORTB4	//INPUT INTERRUPT
+#define INT1_ACC	PORTB2	//INPUT INTERRUPT MOTION DETECTION (PCIE0)
 #define VCC_SOLAR	PORTC0	//INPUT ADC 
 #define BATT_READ	PORTC1	//INPUT ADC 
 #define BR_CNTL		PORTC2	//OUTPUT
-#define INT2_ACC	PORTC3	//INPUT INTERRUPT
+#define INT2_ACC	PORTC3	//INPUT INTERRUPT DATA READY (PCIE1)
 #define LED_CNTL	PORTD2	//OUTPUT PWM
+#define CE_REG1		PORTD4	//OUTPUT
 #define ECHO_3V3	PORTD5	//INPUT
 #define TRIGGER		PORTD6	//OUTPUT
 
-//BIT-BANG I2C PINS
-#define SOFT_SCL	PORTD4	//INOUT
-#define SOFT_SDA	PORTD3	//INOUT
+#define INT1_PCINT	PCINT2
+#define INT2_PCINT	PCINT11
+
+//BIT-BANG I2C PINS (define in i2c.h)
+#define SOFT_SCL	PORTD6	//INOUT
+#define SOFT_SDA	PORTD7	//INOUT
 
 /************************************************************************/
 //ACCELEROMETER CONSTANTS: these all need to be translated to attributes
 /************************************************************************/
-#define ACC1_ADDR				0x1D	// I2C address for first accelerometer
+#define ACCEL_ADDR				0x1D	// I2C address for first accelerometer
 #define SCALE					0x08	// Sets full-scale range to +/-2, 4, or 8g. Used to calc real g values.
 #define DATARATE				0x07	// 0=800Hz, 1=400, 2=200, 3=100, 4=50, 5=12.5, 6=6.25, 7=1.56
 #define SLEEPRATE				0x03	// 0=50Hz, 1=12.5, 2=6.25, 3=1.56
@@ -64,29 +67,29 @@
 #define NUM_AXIS 3
 #define NUM_ACC_DATA (DATARATE * NUM_AXIS)
 
-#define ALL_INTS 3
+#define ALL_INTS 2
 
 typedef enum{
-	F800 = 0,
-	F400,
-	F200,
-	F100,
-	F50,
-	F12,
-	F6,
-	F1
-} ACC_ODR;
+	F80000 = 0,
+	F40000,
+	F20000,
+	F10000,
+	F5000,
+	F1250,
+	F625,
+	F156
+} AccOdr;
 
 
 /************************************************************************/
 //PROTOTYPES
 /************************************************************************/
+void init_accel(void);
 void initialize_pins(void);
 long read_batt(void);
 void clear_acc_ints(void);
 void disable_int(uint8_t pcie);
 void enable_int(uint8_t pcie);
-void set_factory_mode(void);
 void sleep_handler(bool still);
 void setup(void);
 void loop(void);
@@ -95,23 +98,33 @@ void loop(void);
 /************************************************************************/
 //GLOBAL VARIABLES
 /************************************************************************/
-volatile bool got_slp_wake;
-volatile bool got_data_acc;
-volatile bool factory_sleep;
-volatile bool got_int_ble;
-bool accel_on;
-bool _sleep_inactive;
-bool _sleep_active;
+static volatile bool got_slp_wake;
+static volatile bool got_data_acc;
+static volatile bool _sleep;
+static volatile uint8_t intSource;
 
-volatile uint8_t int_source = 0x00;
+static bool accel_on;
+static bool _sleep_active;
 
-uint16_t intCount;
-int accelCount[3];  				// Stores the 12-bit signed value
-float accelG[3];  					// Stores the real accel value in g's
-MMA8452 acc1 = MMA8452(ACC1_ADDR);
+static int16_t accelCount[3];  				// Stores the 12-bit signed value
+static float accelG[3];  						// Stores the real accel value in g's
+AccOdr f_odr = (AccOdr)DATARATE;
+MMA8452 accel = MMA8452(ACCEL_ADDR);
 
 /************************************************************************/
 
+
+//#START_FUNCTION_HEADER//////////////////////////////////////////////////////
+//#
+//# Description: 	Initialization function.  Configures IO pins, enables interrupts.
+//#					Heartbeat checks the accelerometerS.
+//#					in debug, it can print out initialization stats.
+//#
+//# Parameters:		None
+//#
+//# Returns: 		Nothing
+//#
+//#//END_FUNCTION_HEADER////////////////////////////////////////////////////////
 void setup()
 {
 	//initialize the pins
@@ -120,8 +133,7 @@ void setup()
 	sei();								//ENABLE EXTERNAL INTERRUPT FOR WAKEUP
 	got_slp_wake = false;
 	got_data_acc = false;
-	got_int_ble = false;
-	_sleep_inactive = false;
+	_sleep = false;
 	_sleep_active = false;
 	
 	
@@ -131,14 +143,9 @@ void setup()
 	Serial.flush();	 //clear the buffer before sleeping, otherwise can lock up the pipe
 	delay(1000);
 #endif
-	//while(1){
-		//i2cSendStart();
-		//i2cSendByte(0xAA);
-		//i2cSendStop();
-		//delay(100);
-	//}
-	unsigned char temp = acc1.readRegister(WHO_AM_I);
-	if (temp== 0x2A) 						// WHO_AM_I should always be 0x2A
+
+	
+	if (accel.readRegister(WHO_AM_I) == 0x2A) 						// WHO_AM_I should always be 0x2A
 	{
 		accel_on = true;
 
@@ -149,8 +156,6 @@ void setup()
 	else
 	{
 #ifdef RDV_DEBUG
-		Serial.print("0x");
-		Serial.println(temp, HEX);
 		Serial.println("Could not connect to MMA8452Q");
 #endif
 		accel_on = false;
@@ -158,7 +163,7 @@ void setup()
 	
 #ifdef RDV_DEBUG
 	Serial.println("WELCOME TO BLINDSAFE...");
-	Serial.println("+++++++++++++++++++++++++++++");
+	Serial.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 	long tempVcc = read_batt();
 	Serial.print("Power Level At: ");
 	Serial.print(tempVcc / 1000);
@@ -166,81 +171,77 @@ void setup()
 	Serial.print(tempVcc % 1000);
 	Serial.println("V");
 	Serial.print("ACC ODR: ");
-	Serial.println(DATARATE);
+	Serial.println(f_odr);
 	Serial.print("ACC 1: ");
 	Serial.println(accel_on);
 	Serial.println("+++++++++++++++++++++++++++++");
-	Serial.println("Setting into factory mode...");
-	delay(2500);
+	//Serial.println("Setting into factory mode...");
+	delay(500);
 #endif
-	while(1);
-	set_factory_mode();
+	init_accel();
+	
 }
 
 
-// Main loop
+//#START_FUNCTION_HEADER//////////////////////////////////////////////////////
+//#
+//# Description: 	Main loop
+//#
+//# Parameters:		None
+//#
+//# Returns: 		Nothing
+//#
+//#//END_FUNCTION_HEADER////////////////////////////////////////////////////////
 void loop()
 {
-	//BLE SENT INTERRUPT
-	if(got_int_ble)
-	{
-		got_int_ble = false;
-		
-#ifdef RDV_DEBUG
-		Serial.println("Setting up accelerometers...");
-#endif
-		enable_int(ALL_INTS);
-
-		acc1.initMMA8452(SCALE, DATARATE, SLEEPRATE, ASLP_COUNT, MOTION_THRESHOLD, MOTION_DEBOUNCE_COUNT);
-	}
-	
 	//ACCELEROMETER DATA IS READY
 	if (got_data_acc && accel_on)
 	{
-		acc1.readAccelData(accelCount);  // Read the x/y/z adc values, clears int
 		got_data_acc = false;
+		accel.readAccelData(accelCount);  // Read the x/y/z adc values, clears int
+		_sleep_active = true;
+		_sleep = true;
 		
 		// Now we'll calculate the acceleration value into actual g's
-		for (int i=0; i<3; i++)
+		for (uint16_t i=0; i<3; i++)
 		accelG[i] = (float) accelCount[i]/((1<<12)/(2*SCALE));  // get actual g value, this depends on scale being set
 		// Print out values
-
-		for (int i=0; i<NUM_AXIS; i++)
-		{
+		
 #ifdef RDV_DEBUG
+		for (uint16_t i=0; i<NUM_AXIS; i++)
+		{
 			Serial.print(accelG[i], 4);  // Print g values
 			Serial.print("\t");  // tabs in between axes
-#endif
 		}
-#ifdef RDV_DEBUG
 		Serial.println();
 #endif
 	}
-	
-	//ACCELEROMETER WENT INTO SLEEP/AWAKE MODE
+		
+	//ACCELEROMETER CHANGED INTO SLEEP/AWAKE STATE
 	if(got_slp_wake)
 	{
 		got_slp_wake = false;
-		
-		#ifdef RDV_DEBUG
+		intSource= accel.readRegister(INT_SOURCE) & 0xFE; //we don't care about the data interrupt here
+		accel.readRegister(FF_MT_SRC);	//CLEAR MOTION INTERRUPT
+#ifdef RDV_DEBUG
 		Serial.print("INT 1: 0x");
-		Serial.println(int_source, HEX);
-		#endif
-		switch(int_source)
+		Serial.println(intSource, HEX);
+#endif
+		switch(intSource)
 		{
 			case 0x84:		//MOTION AND SLEEP/WAKE INTERRUPT (if even possible?)
 			case 0x80:		//SLEEP/WAKE INTERRUPT
 			{
-				uint8_t sysmod = acc1.readRegister(SYSMOD);
-				//acc1.readRegister(FF_MT_SRC);	//CLEAR MOTION INTERRUPT
+				uint8_t sysmod = accel.readRegister(SYSMOD);
+				//accel.readRegister(FF_MT_SRC);	//CLEAR MOTION INTERRUPT
 #ifdef RDV_DEBUG
 				Serial.print("SYSMOD: 0x");
 				Serial.println(sysmod);
 #endif
 				if(sysmod == 0x02)    		//SLEEP MODE
-				_sleep_inactive = true;
+				_sleep = true;
 				else if(sysmod == 0x01)  	//WAKE MODE
-				_sleep_inactive = false;
+				_sleep = false;
 				break;
 			}
 			case 0x04:						//MOTION INTERRUPT
@@ -248,95 +249,86 @@ void loop()
 			break;
 		}
 #ifdef RDV_DEBUG
-		Serial.println("Clearing ints...");
+		Serial.println("Clearing interrupts...");
 #endif
 		clear_acc_ints();			//clear interrupts at the end of this handler
+		_sleep_active = false;		//sleep without waking up from data interrupt
 	}
+		
+	if(_sleep)
+	{
+#ifdef RDV_DEBUG
+		Serial.flush();	 //clear the buffer before sleeping, otherwise can lock up the pipe
+		delay(10);		//NOTE: THIS WILL MESS UP FUNCTIONAL TIMING, JUST FOR DEBUGGING TO ENSURE FLUSH
+#endif
+		sleep_handler(_sleep_active);
+	}//if(got_slp_wake)
 
-	if(_sleep_inactive)
-	{
-#ifdef RDV_DEBUG
-		Serial.flush();	 //clear the buffer before sleeping, otherwise can lock up the pipe
-		delay(10);
-#endif
-		sleep_handler(true);
-	}
-	else if(_sleep_active)
-	{
-#ifdef RDV_DEBUG
-		Serial.flush();	 //clear the buffer before sleeping, otherwise can lock up the pipe
-		delay(10);
-#endif
-		sleep_handler(false);
-	}
 }// void loop()
-
 
 //#START_FUNCTION_HEADER//////////////////////////////////////////////////////
 //#
-//# Description: 	**NOT USED IN PRODUCTION**
-//#					Puts the blind spot detector into factory mode.  This mode
-//#		 			is essentially an ultra deep sleep mode that is only brought out
-//#					of sleep by one specific interrupt generated.
+//# Description: 	Initializes the accelerometer and enables the interrupts
 //#
 //# Parameters:		None
 //#
 //# Returns: 		Nothing
 //#
 //#//END_FUNCTION_HEADER////////////////////////////////////////////////////////
-void set_factory_mode()
-{
-	got_slp_wake = false;
-	got_data_acc = false;
-	got_int_ble = false;
-	factory_sleep = true;
+void init_accel(){
 	
-	enable_int(PCIE0);
-	disable_int(PCIE1);
-	disable_int(PCIE2);
-	acc1.MMA8452Standby();
+	enable_int(ALL_INTS);
+#ifdef RDV_DEBUG
+	Serial.println("Setting up accelerometers...");
+	Serial.print("Scale: ");
+	Serial.println(SCALE);
+	Serial.print("Data odr: ");
+	Serial.println(DATARATE);
+	Serial.print("Sleep odr: ");
+	Serial.println(SLEEPRATE);
+	Serial.print("Asleep count: ");
+	Serial.println(ASLP_COUNT);
+	Serial.print("Motion threshold: ");
+	Serial.println(MOTION_THRESHOLD);
+	Serial.print("Debounce count: ");
+	Serial.println(MOTION_DEBOUNCE_COUNT);
+	Serial.println("+++++++++++++++++++++++++++++");
+#endif
 	
-	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-	sleep_enable();
-	cli();
-	sleep_bod_disable();
-	sei();
-	sleep_cpu();
-	sleep_disable();
-	clear_acc_ints();
+	accel.initMMA8452(SCALE, DATARATE, SLEEPRATE, ASLP_COUNT, MOTION_THRESHOLD, MOTION_DEBOUNCE_COUNT);
 }
 
 //#START_FUNCTION_HEADER//////////////////////////////////////////////////////
 //#
-//# Description: 	Creates a new deck with a specified number of cards, logging
-//#		 			the cards initial position for comparison after rounds later.
+//# Description: 	Puts the unit into sleep while enabling proper interrupts to
+//#					exit sleep mode.  In normal mode, we want to sleep in between
+//#					data ready acquisitions to maximize power.  When no motion is present,
+//#					we only want to be woken up by BLE or movement again, not data
+//#					ready.
 //#
-//# Parameters:		Deck structure pointer with the specified deck length and pointers
-//#					to the head and tail of the two "piles"
+//# Parameters:		still --> 	true = disable acc data interrupts
+//#								false = enable acc data interrupts
 //#
 //# Returns: 		Nothing
 //#
 //#//END_FUNCTION_HEADER////////////////////////////////////////////////////////
-void sleep_handler(bool still)
+void sleep_handler(bool active)
 {
 	got_slp_wake = false;
 	got_data_acc = false;
-	got_int_ble = false;
-	factory_sleep = false;
 
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 	sleep_enable();
 	cli();
 	sleep_bod_disable();
 	enable_int(PCIE0);
-	enable_int(PCIE1);
-	disable_int(PCIE2);
-	still ? disable_int(PCIE2) : enable_int(PCIE2);	//we want to sleep in between data reads AND when no motion occurs
+	active ? enable_int(PCIE1) : disable_int(PCIE1);	//if we want to sleep in between data reads AND when no motion occurs
 	clear_acc_ints();
 	sei();
 	sleep_cpu();
 	sleep_disable();
-	enable_int(PCIE2);
+	enable_int(PCIE1);
+	_sleep = false;
 }
 
 //#START_FUNCTION_HEADER//////////////////////////////////////////////////////
@@ -352,9 +344,9 @@ void sleep_handler(bool still)
 //#//END_FUNCTION_HEADER////////////////////////////////////////////////////////
 void clear_acc_ints()
 {
-	acc1.readRegister(INT_SOURCE);
-	acc1.readRegister(FF_MT_SRC);
-	acc1.readAccelData(accelCount);
+	accel.readRegister(INT_SOURCE);
+	accel.readRegister(FF_MT_SRC);
+	accel.readAccelData(accelCount);
 }
 
 //#START_FUNCTION_HEADER//////////////////////////////////////////////////////
@@ -369,15 +361,15 @@ void clear_acc_ints()
 void initialize_pins()
 {
 	//outputs
-	DDRB =  (1 << LED_CNTL) | (1 << CE_REG1) | (1 << CE_REG);
+	DDRB =  (1 << CE_REG);
 	DDRC =  (1 << BR_CNTL);
-	DDRD =  (1 << TRIGGER);
+	DDRD =  (1 << TRIGGER) | (1 << CE_REG1) | (1 << LED_CNTL);
 	//inputs
 	DDRB =  ~(1 << INT1_ACC);
 	DDRC =  ~(1 << VCC_SOLAR) & ~(1 << BATT_READ) & ~(1 << INT2_ACC);
 	DDRD =  ~(1 << ECHO_3V3) & ~(1 << SOFT_SDA) & ~(1 << SOFT_SCL);
-	//pwm setup
-	//TODO
+	
+	//TODO: PWM SETUP
 }
 
 //#START_FUNCTION_HEADER//////////////////////////////////////////////////////
@@ -395,32 +387,26 @@ void enable_int(uint8_t pcie)
 	{
 		case PCIE0:
 		{
-			PCMSK0 |= (1 << PCINT4);	//INT1_ACC Interrupt
+			PCMSK0 |= (1 << INT1_PCINT);	//INT1_ACC Interrupt
 			break;
 		}
 		case PCIE1:
 		{
-			PCMSK1 |= (1 << PCINT11);	//INT2_ACC Interrupt
-			break;
-		}
-		case PCIE2:
-		{
-			PCMSK2 |= (1 << PCINT18);	//Undefined
+			PCMSK1 |= (1 << INT2_PCINT);	//INT2_ACC Interrupt
 			break;
 		}
 		default:
 		{
-			PCMSK0 |= (1 << PCINT4);	//BLE Interrupt
-			PCMSK1 |= (1 << PCINT11);	//ACC SLEEP/WAKE interrupt
-			PCMSK2 |= (1 << PCINT18);	//ACC DATA interrupt
+			PCMSK0 |= (1 << INT1_PCINT);
+			PCMSK1 |= (1 << INT2_PCINT);	
 			break;
 		}
 	}
 	
-	if(pcie <= PCIE2)
+	if(pcie <= PCIE1)
 		PCICR |= (1 << pcie);
 	else
-		PCICR |= ((1 << PCIE0) | (1 << PCIE1) | (1 << PCIE2));
+		PCICR |= ((1 << PCIE0) | (1 << PCIE1));
 }
 
 
@@ -439,29 +425,23 @@ void disable_int(uint8_t pcie)
 	{
 		case PCIE0:
 		{
-			PCMSK0 &= ~(1 << PCINT4);	//INT1_ACC Interrupt
+			PCMSK0 &= ~(1 << INT1_PCINT);	//INT1_ACC Interrupt
 			break;
 		}
 		case PCIE1:
 		{
-			PCMSK1 &= ~(1 << PCINT11);	//INT2_ACC Interrupt
-			break;
-		}
-		case PCIE2:
-		{
-			PCMSK2 &= ~(1 << PCINT18);	//Undefined
+			PCMSK1 &= ~(1 << INT2_PCINT);	//INT2_ACC Interrupt
 			break;
 		}
 		default:
 		{
-			PCMSK0 &= ~(1 << PCINT4);	
-			PCMSK1 &= ~(1 << PCINT11);	
-			PCMSK2 &= ~(1 << PCINT18);	
+			PCMSK0 &= ~(1 << INT1_PCINT);	
+			PCMSK1 &= ~(1 << INT2_PCINT);	
 			break;
 		}
 	}
 	
-	if(pcie <= PCIE2)
+	if(pcie <= PCIE1)
 		PCICR &= ~(1 << pcie);
 	else
 		PCICR = 0;
@@ -489,7 +469,7 @@ long read_batt()
 	
 	delay(2); // Wait for Vref to settle
 	ADCSRA |= _BV(ADSC); // Start conversion
-	while (ADCSRA & _BV(ADSC)); // measuring
+	while (ADCSRA & _BV(ADSC)); 
 	
 	uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
 	uint8_t high = ADCH; // unlocks both
@@ -506,40 +486,36 @@ long read_batt()
 
 //#START_FUNCTION_HEADER//////////////////////////////////////////////////////
 //#
-//# Description: 	Creates a new deck with a specified number of cards, logging
-//#		 			the cards initial position for comparison after rounds later.
+//# Description: 	ISR for the PCINT0 bus interrupt.  This is an external interrupt from
+//#					the accelerometer that is triggered by filtered motion or if the
+//#					accelerometer is entering or exiting sleep mode.
 //#
-//# Parameters:		Deck structure pointer with the specified deck length and pointers
-//#					to the head and tail of the two "piles"
+//# Parameters:		Interrupt vector
 //#
 //# Returns: 		Nothing
 //#
-//#//END_FUNCTION_HEADER//////////////////////////////////////////////////////
-ISR(PCINT1_vect)
+//#//END_FUNCTION_HEADER////////////////////////////////////////////////////////
+ISR(PCINT0_vect)
 {
 	cli();
 	if(digitalRead(INT1_ACC))
 	{
-		int_source= acc1.readRegister(INT_SOURCE) & 0xFE; //we don't care about the data interrupt here
-		acc1.readRegister(FF_MT_SRC);	//CLEAR MOTION INTERRUPT
 		got_slp_wake = true;
 	}
 	sei();
 }
 
-
 //#START_FUNCTION_HEADER//////////////////////////////////////////////////////
 //#
-//# Description: 	Creates a new deck with a specified number of cards, logging
-//#		 			the cards initial position for comparison after rounds later.
+//# Description: 	ISR for the PCINT1 bus interrupt.  This is an external interrupt from
+//#					the accelerometer that is triggered by the accelerometer data being ready.
 //#
-//# Parameters:		Deck structure pointer with the specified deck length and pointers
-//#					to the head and tail of the two "piles"
+//# Parameters:		Interrupt vector
 //#
 //# Returns: 		Nothing
 //#
 //#//END_FUNCTION_HEADER////////////////////////////////////////////////////////
-ISR(PCINT2_vect)
+ISR(PCINT1_vect)
 {
 	cli();
 	if(digitalRead(INT2_ACC))
